2c4f0527b8b417fb25ec238e0f57b277cd1cd5de
mark-nikerin <mrnikerin@gmail.com>
2020-10-10 15:20:13 +0400

    Add saving cars in memory

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 import jsonpickle
 
@@ -25,6 +26,8 @@ probabilities_keys = [
     'Toyota Camry'
 ]
 
+cars = []
+
 
 class Car:
 
@@ -42,54 +45,52 @@ class Car:
 
 @app.route('/marketplace', methods=['GET'])
 def marketplace():
+    if (len(cars) == 0):
+        url = 'https://gw.hackathon.vtb.ru/vtb/hackathon/marketplace'
+        headers = {
+            'x-ibm-client-id': api_key,
+            'accept': "application/json"
+        }
+        response = requests.get(url, headers=headers)
+        jsonresponse = json.loads(response.text)
 
-    url = 'https://gw.hackathon.vtb.ru/vtb/hackathon/marketplace'
-    headers = {
-        'x-ibm-client-id': api_key,
-        'accept': "application/json"
-    }
-    response = requests.get(url, headers=headers)
-    jsonresponse = json.loads(response.text)
-
-    cars = []
-
-    for mark in jsonresponse["list"]:
-        _markTitle = mark["title"]
-        _country = mark["country"]["title"]
-        for model in mark["models"]:
-            _price = model["minPrice"]
-            _modelTitle = model["title"]
-            _colorsCount = model["colorsCount"]
-            for body in model["bodies"]:
-                _bodyAlias = body["alias"]
-                _bodyType = body["type"]
-                _bodyTitle = body["title"]
-                _doorsCount = body["doors"]
-                try:
-                    _widgetPhoto = model["renderPhotos"]["render_widget_right"][_bodyAlias]["path"]
-                    _mainPhoto = model["renderPhotos"]["main"][_bodyAlias]["path"]
-                except:
+        for mark in jsonresponse["list"]:
+            _markTitle = mark["title"]
+            _country = mark["country"]["title"]
+            for model in mark["models"]:
+                _price = model["minPrice"]
+                _modelTitle = model["title"]
+                _colorsCount = model["colorsCount"]
+                for body in model["bodies"]:
+                    _bodyAlias = body["alias"]
+                    _bodyType = body["type"]
+                    _bodyTitle = body["title"]
+                    _doorsCount = body["doors"]
                     try:
-                        _widgetPhoto = model["renderPhotos"]["render_widget_right"]["hatchback"]["path"]
-                        _mainPhoto = model["renderPhotos"]["main"]["hatchback"]["path"]
+                        _widgetPhoto = model["renderPhotos"]["render_widget_right"][_bodyAlias]["path"]
+                        _mainPhoto = model["renderPhotos"]["main"][_bodyAlias]["path"]
                     except:
                         try:
-                            _widgetPhoto = model["renderPhotos"]["render_widget_right"]["wagon"]["path"]
-                            _mainPhoto = model["renderPhotos"]["main"]["wagon"]["path"]
+                            _widgetPhoto = model["renderPhotos"]["render_widget_right"]["hatchback"]["path"]
+                            _mainPhoto = model["renderPhotos"]["main"]["hatchback"]["path"]
                         except:
                             try:
-                                _widgetPhoto = model["renderPhotos"]["render_widget_right"]["crossover"]["path"]
-                                _mainPhoto = model["renderPhotos"]["main"]["crossover"]["path"]
+                                _widgetPhoto = model["renderPhotos"]["render_widget_right"]["wagon"]["path"]
+                                _mainPhoto = model["renderPhotos"]["main"]["wagon"]["path"]
                             except:
                                 try:
-                                    _widgetPhoto = model["renderPhotos"]["render_widget_right"]["hatchback5d"]["path"]
-                                    _mainPhoto = model["renderPhotos"]["main"]["hatchback5d"]["path"]
+                                    _widgetPhoto = model["renderPhotos"]["render_widget_right"]["crossover"]["path"]
+                                    _mainPhoto = model["renderPhotos"]["main"]["crossover"]["path"]
                                 except:
-                                    _widgetPhoto = ""
-                                    _mainPhoto = ""
-                car = Car(_markTitle, _country, _price, _modelTitle, _colorsCount,
-                          _bodyTitle, _doorsCount, _widgetPhoto, _mainPhoto)
-                cars.append(car)
+                                    try:
+                                        _widgetPhoto = model["renderPhotos"]["render_widget_right"]["hatchback5d"]["path"]
+                                        _mainPhoto = model["renderPhotos"]["main"]["hatchback5d"]["path"]
+                                    except:
+                                        _widgetPhoto = ""
+                                        _mainPhoto = ""
+                    car = Car(_markTitle, _country, _price, _modelTitle, _colorsCount,
+                            _bodyTitle, _doorsCount, _widgetPhoto, _mainPhoto)
+                    cars.append(car)
 
     jsonpickle.set_preferred_backend('json')
     jsonpickle.set_encoder_options('json', ensure_ascii=False)
